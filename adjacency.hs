f8e0c7d6de5cdf7a67044f327a69deacb31e600d
Christopher Feener <christopher.feener@wsu.edu>
2018-09-06 17:31:13 -0700

    Learn Haskell, and write ideas.

---
+++ adjacency.hs
@@ -0,0 +1,7 @@
+-- To check if a graph is planar:
+
+--    Theorem 1. e ≤ 3v − 6;
+--    Theorem 2. If there are no cycles of length 3, then e ≤ 2v − 4.
+--    Theorem 3. f ≤ 2v − 4.
+
+main = do putStrLn "In construction!"
