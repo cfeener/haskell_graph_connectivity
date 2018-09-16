6107e82ccc1259b8743c71de86c0d302efe3e267
Christopher Feener <christopher.feener@wsu.edu>
2018-09-16 16:54:04 -0700

    Start working on using Graph library functions.

--- adjacency.hs
+++ adjacency.hs
@@ -4,4 +4,25 @@
 --    Theorem 2. If there are no cycles of length 3, then e ≤ 2v − 4.
 --    Theorem 3. f ≤ 2v − 4.
 
-main = do putStrLn "In construction!"
+{- isPlanar :: Integer -> Integer -> Bool
+isPlanar e v = if e <= 3*v - 6  -}
+
+-- TASK ONE: Check for 3-cycles
+-- From https://www.schoolofhaskell.com/school/to-infinity-and-beyond/pick-of-the-week/Simple%20examples#data-graph
+
+import Data.Graph
+
+g = buildG (1, 6) [(1, 2), (1, 3), (2, 4), (5, 6)]
+
+main = do
+	print g
+	print $ vertices g
+	print $ edges g
+
+--	print $ topSort g
+
+	print $ path g 1 4
+	print $ path g 1 5
+
+--main = do putStrLn "In construction!"
+
